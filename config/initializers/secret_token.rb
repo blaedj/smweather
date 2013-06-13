ff3ed20c18ddcb836de492dd3764009dffe9f98c
Blaed <blaedj@gmail.com>
2013-06-13 16:18:47 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smweather::Application.config.secret_token = '00f49be660a16a1e0626785a0633ad91abe6f16b6c97de73acb10809061c207aa12162de48fd86ee3fe3cabece524a770f6763eb3750247bffa18b105d7c62c7'
