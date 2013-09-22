f917c42dd8b1fb53166bd681cbb904f9e90117f9
Blaed <blaedj@gmail.com>
2013-09-21 23:26:55 -0500

    need to require twilio gem

--- app/controllers/text_reciever_controller.rb
+++ app/controllers/text_reciever_controller.rb
@@ -1,3 +1,5 @@
+require 'twilio-ruby'
+
 class TextRecieverController < ApplicationController
 
   def smsRecieve
@@ -13,25 +15,6 @@ class TextRecieverController < ApplicationController
 
     @parser = parseMessage message_body unless !message_body
 
-    # the following is the format of the Twilio post (json)
-    # {
-    #   "AccountSid"=>"asdf876a87f87a6sdf876876asd8f76a8sdf595asdD",
-    #   "Body"=> body,
-    #   "ToZip"=>"94949",
-    #   "FromState"=>"MI",
-    #   "ToCity"=>"NOVATO",
-    #   "SmsSid"=>"asd8676585a78sd5f548a64sd4f64a467sg4g858",
-    #   "ToState"=>"CA",
-    #   "To"=>"5555992673",
-    #   "ToCountry"=>"US",
-    #   "FromCountry"=>"US",
-    #   "SmsMessageSid"=>"hjk87h9j8k79hj8k7h97j7k9hj8k7",
-    #   "ApiVersion"=>"2008-08-01",
-    #   "FromCity"=>"GRAND RAPIDS",
-    #   "SmsStatus"=>"received",
-    #   "From"=>"5555992673",
-    #   "FromZip"=>"49507"
-    # }
     twilio_sid = ENV['TWIL_SID'] || raise("No twilio id environment variable set!")
     twilio_token = ENV['TWIL_TOKEN']  || raise("No twilio token environment variable set!")
     twilio_phone_number = ['TWIL_NUM']  || raise("No twilio phone number  environment variable set!")
