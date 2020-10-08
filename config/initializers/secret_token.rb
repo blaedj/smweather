e3e872dcf54374a303648b94696e9154938e0715
blaedj <blaedj@users.noreply.github.com>
2020-10-08 09:47:45 -0500

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,6 +4,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Smweather::Application.config.secret_token = '00f49be660a16a1e0626785a0633ad91abe6f16b6c97de73acb10809061c207aa12162de48fd86ee3fe3cabece524a770f6763eb3750247bffa18b105d7c62c7'
+Smweather::Application.config.secret_token = ENV['SECRET_TOKEN']
 # the above term is deprecated, use secret_key_base now for rails4
-Smweather::Application.config.secret_key_base = 'zzz00f49be660a16a1e0626785a0633ad91abe6f16b6c97de73acb10809061c207aa12162de48fd86ee3fe3cabece524a770f6763eb3750247bffa18b105d7c62c7'
+Smweather::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
